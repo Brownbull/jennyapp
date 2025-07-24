7bb79d992272d80fdaeed24245940d7924d7e4e9
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-07-24 16:47:05 -0400

    refactoring templates

--- jennyapp/blueprints/index.py
+++ jennyapp/blueprints/index.py
@@ -3,4 +3,4 @@ from flask import Blueprint, render_template
 index_bp = Blueprint('index', __name__)
 @index_bp.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index/index.html')
